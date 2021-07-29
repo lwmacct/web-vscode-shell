37e3d5b44f84a64d3f1c903615c346eef4fdebe6
lwmacct <lwmacct@163.com>
2021-07-29 21:13:53 +0800

    init

--- workspace/shell/centos/kernel/sysctl.sh
+++ workspace/shell/centos/kernel/sysctl.sh
@@ -2,7 +2,7 @@
 
 curl -o /etc/sysctl.d/98-sysctl.conf https://gitee.com/lwmacct/web-vscode-shell/raw/main/workspace/shell/centos/kernel/98-sysctl.conf
 
-/sbin/sysctl -p
+/sbin/sysctl -p /etc/sysctl.d/98-sysctl.conf
 
 __run() {
     bash -c "$(curl -sS https://gitee.com/lwmacct/web-vscode-shell/raw/main/workspace/shell/centos/kernel/sysctl.sh)"
