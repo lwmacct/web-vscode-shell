feaced0002b8036462da1f934ebdfaa65d3d6ad6
lwmacct <lwmacct@163.com>
2021-08-29 05:42:34 +0800

    init

--- workspace/shell/centos/network/ipv4_static_bgp_shunt.sh
+++ workspace/shell/centos/network/ipv4_static_bgp_shunt.sh
@@ -106,6 +106,7 @@ __read_config() {
 }
 
 __mian() {
+    ip a | grep -Eo 'vinc.static.[0-9]{3}' | sort -u | xargs -n1 -I {} echo 'ip link set {} down;  ip link del dev {}' | sh
     _f_ip_info=/data/network/ipv4_static.txt
     __read_config
 }
