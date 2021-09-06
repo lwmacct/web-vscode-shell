4c0312f09bd1b817cb3bcfa93a8ce122955ac1fc
lwmacct <lwmacct@163.com>
2021-09-07 03:30:11 +0800

    init

--- workspace/shell/centos/network/ipv4_static_shunt.sh
+++ workspace/shell/centos/network/ipv4_static_shunt.sh
@@ -78,7 +78,7 @@ __set_manage_route_table() {
     # 设置管理线路的路由表
     _is=$(ip route list table 252-manage | grep default -c)
     if ((_is != 1)); then
-        if (($(grep '252-manage' -c </etc/iproute2/rt_tables) == 1)); then echo '252     252-manage' >>/etc/iproute2/rt_tables; fi
+        if (($(grep '252-manage' -c </etc/iproute2/rt_tables) == 0)); then echo '252     252-manage' >>/etc/iproute2/rt_tables; fi
         _nic=$(ip r | grep -v -E 'ppp|docker|br|eth9|vnic' | grep -E '(^default)|(\ssrc\s)' | grep -o 'dev\s\S*' | awk '{print $NF}' | head -1)
         _subnet=$(ip r | grep "$_nic" | grep '.*/\S*\sdev' | head -1 | awk '{print $1}')
         _gateway=$(ip r | grep "default.* dev\s$_nic" | head -1 | awk '{print $3}')
