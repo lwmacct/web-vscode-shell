7444b56ba32254754b2c87dcbb01cfbaeaaa7bc2
lwmacct <lwmacct@163.com>
2021-09-07 02:50:49 +0800

    init

--- workspace/shell/centos/network/ipv4_static_shunt.sh
+++ workspace/shell/centos/network/ipv4_static_shunt.sh
@@ -80,17 +80,17 @@ __set_manage_route_table() {
     if ((_is != 1)); then
         if (($(grep '252-manage' -c </etc/iproute2/rt_tables) == 1)); then echo '252     252-manage' >>/etc/iproute2/rt_tables; fi
         _nic=$(ip r | grep -v -E 'ppp|docker|br|eth9|vnic' | grep -E '(^default)|(\ssrc\s)' | grep -o 'dev\s\S*' | awk '{print $NF}' | head -1)
-        _wd=$(ip r | grep "$_nic" | grep '.*/\S*\sdev' | head -1 | awk '{print $1}')
-        _wg=$(ip r | grep "default.* dev\s$_nic" | head -1 | awk '{print $3}')
-        if [[ "${_wg}" == "" ]]; then
-            _wg=$(cat /etc/sysconfig/network-scripts/ifcfg-"$_nic" | grep -i 'GATEWAY' | awk -F '=' '{print $NF}' | grep -Eo '[0-9.]{1,16}')
+        _subnet=$(ip r | grep "$_nic" | grep '.*/\S*\sdev' | head -1 | awk '{print $1}')
+        _gateway=$(ip r | grep "default.* dev\s$_nic" | head -1 | awk '{print $3}')
+        if [[ "${_gateway}" == "" ]]; then
+            _gateway=$(cat /etc/sysconfig/network-scripts/ifcfg-"$_nic" | grep -i 'GATEWAY' | awk -F '=' '{print $NF}' | grep -Eo '[0-9.]{1,16}')
         fi
-        echo "$_nic $_wd $_wg"
-        if [[ "$_wd" != "" && "$_wg" != "" ]]; then
+        echo "$_nic $_subnet $_gateway"
+        if [[ "$_subnet" != "" && "$_gateway" != "" ]]; then
             ip rule del lookup 252-manage 2>/dev/null
-            ip rule add from "$_wd" table 252-manage
+            ip rule add from "$_subnet" table 252-manage
             ip route flush table 252-manage
-            ip route add default via "$_wg" table 252-manage
+            ip route add default via "$_gateway" table 252-manage
             ip route list table 252-manage
             echo 'table set'
         fi
@@ -135,6 +135,7 @@ __mian() {
     ip a | grep -Eo 'vnic.static.[0-9]{3}' | sort -u | xargs -n1 -I {} echo 'ip link set {} down;  ip link del dev {}' | sh
     _f_ip_info=/data/network/ipv4_static.txt
     __read_config
+
     __set_static_ip_route
 }
 __mian
